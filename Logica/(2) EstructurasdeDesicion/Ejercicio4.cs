e1d15388d66ec38249b364c7c84b4bf15762dec5
JesusTrz <yisusgonzalez2315@gmail.com>
2026-08-08 12:41:14 -0600

    Ejercicio 4

--- Logica/(2) EstructurasdeDesicion/Ejercicio4.cs
+++ Logica/(2) EstructurasdeDesicion/Ejercicio4.cs
@@ -3,6 +3,27 @@ public class EstructuraDecicionEjercicio4 : EjercicioBase
 	public void Ejercicio() 
 	{
 		base.PrepararConsola();
-		System.Console.WriteLine("oooo");
+		System.Console.WriteLine("Verificar si un numero esta dentro o fuera de un dos rangos establecidos.");
+		System.Console.WriteLine("====================================================================");
+		double R1, R2, R3, R4, num;
+		System.Console.WriteLine("(1) Ingresa el primer numero del rango: ");
+		R1 = double.Parse(System.Console.ReadLine()!);
+		System.Console.WriteLine("(1) Ingresa el segundo numero del rango: ");
+		R2 = double.Parse(System.Console.ReadLine()!);
+		System.Console.WriteLine("Primer rango Definido: " + R1 + " - " + R2);
+		System.Console.WriteLine("(2) Ingresa el primer numero del rango: ");
+		R3 = double.Parse(System.Console.ReadLine()!);
+		System.Console.WriteLine("(2) Ingresa el segundo numero del rango: ");
+		R4 = double.Parse(System.Console.ReadLine()!);
+		System.Console.WriteLine("Segundo rango Definido: " + R3 + " - " + R4);
+		System.Console.WriteLine("Escribe un Número: ");
+		num = double.Parse(System.Console.ReadLine()!);
+		if(num > R1 && num < R2 || num > R3 && num < R4)
+		{
+			System.Console.WriteLine("El numero " + num + "Esta dentro del Rango");
+		} else {
+			System.Console.WriteLine("El numero " + num + " Esta fuera del Rango");
+		}
+
 	}
 }
--- Logica/obj/Debug/net10.0/Logica.AssemblyInfo.cs
+++ Logica/obj/Debug/net10.0/Logica.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Logica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82259c418e3f2151a73279b80b1b055668c4f453")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0b2190334874cc9bb1ddf665507c3c0daa49664")]
 [assembly: System.Reflection.AssemblyProductAttribute("Logica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Logica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
